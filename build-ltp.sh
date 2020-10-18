f5e05d42ae7ccc896a3450074c14718dc80a50f6
Anders Roxell <anders.roxell@linaro.org>
2020-10-18 11:02:46 +0200

    cleanup build-* packages
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- build-ltp.sh
+++ build-ltp.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-LTP_VERSION=20200930
-apt install -y git
+RELEASE_VERSION="20200930"
+RELEASE_TESTSUITE_NAME="ltp-full-"${RELEASE_VERSION}".tar.xz"
+RELEASE_TAR_URL="https://github.com/linux-test-project/ltp/releases/download/"${RELEASE_VERSION}"/"${RELEASE_TESTSUITE_NAME}""
+SRCDIR="/opt/builddir/"
+
+# Pre-install packages needed
+apt install -y git wget
 apt install -y xz-utils flex bison build-essential curl net-tools quota genisoimage sudo libaio-dev expect automake acl
 
-git clone https://github.com/linux-test-project/ltp.git
-pushd ltp
-git checkout "${LTP_VERSION}"
+mkdir -p "${SRCDIR}"
+pushd "${SRCDIR}"
+wget "${RELEASE_TAR_URL}"
+tar --strip-components=1 -Jxf "${RELEASE_TESTSUITE_NAME}"
 
+# Building
 ./configure
 make -j8 all
 make SKIP_IDCHECK=1 install
 
 popd
-rm -rf ltp
+rm -rf "${SRCDIR}"
 
-
-sudo apt --purge remove -y git build-essential
+sudo apt --purge remove -y git wget build-essential
--- build-rteval.sh
+++ build-rteval.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+# Only until rt-tests will be using the latest rt-tests release as a deb package.
+REPO_VERSION="v1.8"
+REPO_URL="https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git"
+SRCDIR="/opt/builddir/"
+
+# Pre-install packages needed
 apt install -y git build-essential libnuma-dev python3-dev
 
-git clone https://git.kernel.org/pub/scm/utils/rt-tests/rt-tests.git
-pushd rt-tests
-git checkout v1.8
+
+git clone "${REPO_URL}" "${SRCDIR}"
+pushd "${SRCDIR}"
+git checkout "${REPO_VERSION}"
+
+# Building
 make
 make install
 popd
-rm -rf rt-tests
+rm -rf "${SRCDIR}"
 
+# Installing rteval
+
+# Pre-install packages needed
 apt install -y curl git python3-dev python3-schedutils python3-ethtool python3-lxml python3-dmidecode rt-tests sysstat xz-utils bzip2 tar numactl build-essential flex bison bc elfutils openssl libssl-dev cpio libelf-dev binutils linux-libc-dev keyutils libaio-dev attr libpcap-dev lksctp-tools zlib1g-dev util-linux
 
 openssl req -new -nodes -utf8 -sha256 -days 36500 -batch -x509 -config x509.genkey -outform PEM -out kernel_key.pem -keyout kernel_key.pem || true
-git clone https://git.kernel.org/pub/scm/utils/rteval/rteval.git
-pushd rteval
+
+#REPO_VERSION="master"
+REPO_URL="https://git.kernel.org/pub/scm/utils/rteval/rteval.git"
+SRCDIR="/opt/rteval/"
+
+git clone "${REPO_URL}" "${SRCDIR}"
+pushd "${SRCDIR}"
 git log --oneline -1 >.rteval_git_version
 popd
+
+sudo apt --purge remove -y git
