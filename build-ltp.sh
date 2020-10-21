9310d90895cc2e52fb4f66033a4dfedda7263371
Anders Roxell <anders.roxell@linaro.org>
2020-10-21 13:50:14 +0200

    build-ltp: add packages and build from git
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- build-ltp.sh
+++ build-ltp.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 RELEASE_VERSION="20200930"
-RELEASE_TESTSUITE_NAME="ltp-full-"${RELEASE_VERSION}".tar.xz"
-RELEASE_TAR_URL="https://github.com/linux-test-project/ltp/releases/download/"${RELEASE_VERSION}"/"${RELEASE_TESTSUITE_NAME}""
+REPO_URL="https://github.com/linux-test-project/ltp.git"
 SRCDIR="/opt/builddir/"
 
 # Pre-install packages needed
 apt install -y git wget
-apt install -y xz-utils flex bison build-essential curl net-tools quota genisoimage sudo libaio-dev expect automake acl m4 libattr1-dev libcap-dev autotools-dev autoconf
+apt install -y xz-utils flex bison build-essential curl net-tools quota genisoimage sudo libaio-dev expect automake acl m4 libattr1-dev libcap-dev autotools-dev autoconf pkgconf
 
-mkdir -p "${SRCDIR}"
+git clone "${REPO_URL}" "${SRCDIR}"
 pushd "${SRCDIR}"
-wget "${RELEASE_TAR_URL}"
-tar --strip-components=1 -Jxf "${RELEASE_TESTSUITE_NAME}"
+git checkout "${REPO_VERSION}"
 
 # Building
+make autotools
 ./configure
 make -j8 all
 make SKIP_IDCHECK=1 install
